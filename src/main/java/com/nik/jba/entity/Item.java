5787c1aefbc77eaebfc3e44cb820f1644028827d
Nikhil Sharma <nik.neal91@gmail.com>
2015-07-03 00:13:07 +0530

    Created all persistance along with relationships

---
+++ src/main/java/com/nik/jba/entity/Item.java
@@ -0,0 +1,75 @@
+package com.nik.jba.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String title;
+	
+	@ManyToOne
+	@JoinColumn(name="blog_id")
+	private Blog blog;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	private String link;
+	
+	
+	@Column(name="pulished_date")
+	private Date publishedDate;
+	
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
